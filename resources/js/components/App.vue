c3f0eb8b333f402f66fc94a8af677cf9c578f437
Ruben Danielyan <rub1994.13@gmail.com>
2021-05-20 07:01:41 +0400

    Install vuetify and laravel ui

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <v-app>
+            <v-alert type="error" :value="true">
+                Vuetify was installed properly
+            </v-alert>
+        </v-app>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
